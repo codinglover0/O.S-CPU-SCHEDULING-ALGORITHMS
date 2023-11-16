932f89698ea373ddb6e06337a744256bad8edc18
Sarthak Gupta <118931056+codinglover0@users.noreply.github.com>
2023-11-17 00:50:06 +0530

    Update Shortest  Job First ( SJF ) - Non-Preemptive.py

--- Shortest  Job First ( SJF ).py
+++ Shortest  Job First ( SJF ).py
@@ -1,73 +1,86 @@
-                       #SJF ALGORITHM 
+                                                                    #SJF ( NON-PREEMPTIVE)
+
+#If arrival time either starts with zero(0) or not.
+
 
 '''a is a list containing 5 lists. list a[0] denotes Arrival time(at) , a[1]
 denotes Burst time(bt) , a[2] denotes Completion time(ct) , a[3] denotes
 Turnaround time(tat) and a[4] denotes Waiting time(wt).'''
-     
-a=[[0,2,5,15,10],[5,9,7,6,3],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-at=a[0]
-bt=a[1]
 
-#at1 is sorted arrival time list and bt1 is list of corresponding burst time.
-at1=[0,2,5,10,15]
-bt1=[5,9,7,3,6]
-k=[] 
-l=[]
 
-#gc is a variable initialising it to zero.
-gc=0
-gc=gc+a[1][0]
-a[2][0]=gc
-a[3][0]=a[2][0]-a[0][0]
-a[4][0]=a[3][0]-a[1][0]
-at1.remove(at1[0])
-bt1.remove(bt1[0])
+a=[]
+lst=[]
 
-while(len(at1)>0):
-    for i in range(len(at1)):
-        if(at1[i]<=gc):
-            l.append(at1[i])
-            k.append(at.index(l[i]))
-            c=[bt[i] for i in k]     #appending burst time in list c, of index k in list bt
-    m=min(c)
-    gc=gc+m
-    n=bt.index(m)
-    a[2][n]=gc
-    a[3][n]=a[2][n]-a[0][n]
-    a[4][n]=a[3][n]-a[1][n]
-    at1.remove(at[n])
-    bt1.remove(bt[n])
-    k=[]
-    l=[]                    
+n = int(input("Enter number of processes : "))
 
-#for printing at,tb,ct.tat,and wt in tabular format    
+for i in range(2):
+    print("enter elements in list: ",i+1)                           
+    for j in range(0, n):
+        ele = int(input())
+        # adding the element
+        lst.append(ele)
+    a.append(lst)
+    lst=[]     
+
+for i in range(3):
+    for i in range(n):
+        lst.append(0)
+    a.append(lst)   
+    lst=[]    
+
+#bt1 is unsorted list of burst time ,bt is sorted list of burst time and at is sorted list of arrival time.
+bt1=a[1]
+#gc is a variable initialising it to zero
+at=sorted(a[0])
+i=0
+bt=sorted(a[1])
+g_c=at[0]
+i=a[0].index(g_c)
+g_c=g_c+bt1[i]
+b=bt1[i]
+a[2][i]=g_c
+a[3][i]=a[2][i]-a[0][i]
+a[4][i]=a[3][i]-a[1][i]
+
+i=0
+while(i!=n):
+    m=bt[i]
+    j=bt1.index(m)
+    if(m!=b):
+        g_c=g_c+m
+        a[2][j]=g_c
+        a[3][j]=a[2][j]-a[0][j]
+        a[4][j]=a[3][j]-a[1][j]
+    i+=1
+
+
+#for printing at,tb,ct,tat and wt in tabular format    
 print('at\tbt\tct\ttat\twt')
-for i in range(5):    
-    print(a[i][0],end='\t')
-print(end='\n')
-for i in range(5):    
-    print(a[i][1],end='\t')
-print(end='\n')  
-for i in range(5):  
-    print(a[i][2],end='\t')        
-print(end='\n')   
-for i in range(5):
-    print(a[i][3],end='\t')
-print(end='\n') 
-for i in range(5):
-    print(a[i][4],end='\t') 
-print(end='\n')
+for i in range(n):
+    for j in range(5):    
+        print(a[j][i],end='\t')
+    print(end='\n')
+       
 
-#Calculating average waiting time
+#calculating average Turn Around time and Waiting time
 avg_wt=0
-for i in range(5):
+avg_tat=0
+for i in range(n):    
+    avg_tat=avg_tat+a[3][i]
+print('The average turn around time is: ',avg_tat/n)    
+for i in range(n):    
     avg_wt=avg_wt+a[4][i]
-print('the average waiting time is: ' ,avg_wt/5)
-    
+print('The average waiting time is: ',avg_wt/n)    
+
+
+        
 
 
 
-    
+
+
+
+
 
 
 
